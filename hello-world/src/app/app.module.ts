f2e9139f2e1a5e7f9252f1ca26d65e037bd4cd0e
Tung Pham <phamlamtung@gmail.com>
2017-11-27 17:24:39 -0800

    binding in Angular

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,6 +1,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -10,16 +11,20 @@ import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
 import { AuthorsService } from './authors.service';
 import { AuthorsComponent } from './authors/authors.component';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    SummaryPipe
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     CoursesService,
--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -5,18 +5,59 @@ import { CoursesService } from './courses.service';
     selector: 'courses',
     template: `
         <h2>{{ getTitle() }}</h2>
+        <h2 [textContent] = "title"></h2>
         <ul>
             <li *ngFor = "let course of courses">
                 {{course}}
             </li>
-        </ul>       
+        </ul>  
+        <img src = {{imgUrl}} />
+        <img [src] = "imgUrl" />  
+        <table>
+            <tr>
+                <td [attr.colspan] = "colSpan"></td>
+            </tr>
+        </table>  
+        <div (click) = "onDivClicked()">
+            <button class="btn btn-primary" [class.active] = "isActive" 
+            [style.backgroundColor] = "isActive? 'blue': 'white'"
+            (click) = "onSave($event)"
+            >Save</button>
+        </div>
+
+        <input (keyup) = "onKeyUp($event)" />
+        <input [(ngModel)] = "email" (keyup.enter) = "onEnter()" />       
+        <div>
+            {{course.title | uppercase }} <br/>
+            {{course.rating | number: '1.1-1'}} <br/>
+            {{course.students | number }} <br/>
+            {{course.price | currency: 'HKD': true: '3.2-2'}} <br/>
+            {{course.releaseDate| date:'dd-MM-yyyy'}}
+        </div>    
+        
+        {{ text | summary: 10}}
         ` 
 })
 export class CoursesComponent {
 
     title = 'List of Courses';
     courses;
+    imgUrl = 'http://lorempixel.com/400/200';
+    colSpan = 2;  
+    isActive = true;
+    email = "tung.fpt@gmail.com";
 
+    text = `Lorem Ipsum is simply dummy sentences Lorem Ipsum is simply dummy sentencesLorem Ipsum is simply dummy sentencesLorem Ipsum is simply dummy sentencesLorem Ipsum is simply dummy sentencesLorem Ipsum is simply dummy sentencesLorem Ipsum is simply dummy sentencesLorem Ipsum is simply dummy sentencesLorem Ipsum is simply dummy sentencesLorem Ipsum is simply dummy sentencesLorem Ipsum is simply dummy sentencesLorem Ipsum is simply dummy sentences
+    `
+
+    course = {
+        title: "The complete Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016, 3, 1)
+    };
+    
     getTitle() {
         return this.title;
     }
@@ -24,5 +65,27 @@ export class CoursesComponent {
     constructor (coursesService: CoursesService) {
 
         this.courses = coursesService.getCourses();
+    }
+
+    onSave($event) {
+        $event.stopPropagation();
+        console.log ("Button was clicked", $event);
+    }
+
+    onDivClicked() {
+        console.log ("Div was clicked");
+    }
+
+    onKeyUp($event) {
+        if($event.keyCode === 13){
+            console.log("Enter was pressed");
+            console.log($event.target.value);
+        } 
+    }
+
+    onEnter(email) {
+        console.log("Enter was pressed");
+        console.log(this.email);
+
     }
 }
