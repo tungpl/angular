01192214e38451fea853efd449a30fb3b7009931
Tung Pham <phamlamtung@gmail.com>
2018-06-12 23:14:20 -0700

    [my-course-project] Navbar and layout recipe-list

--- my-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+  public recipes: Recipe[] = [
+    new Recipe("Test recipe", "This is the test description", "https://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg")
+  ];
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
