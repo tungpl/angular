c699890d1d1e18b2565a881fc2b62dc76e5aa1b0
tungpl <phamlamtung@gmail.com>
2018-06-13 19:05:34 -0700

    [my-course-project] Complete Component and Databinding with course project

--- my-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recSelected = new EventEmitter<Recipe>();
   public recipes: Recipe[] = [
     new Recipe("Test recipe", "This is the test description", "https://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg"),
-    new Recipe("Test recipe", "This is the test description", "https://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg")
+    new Recipe("Another Test recipe", "This is the test description", "https://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg")
   ];
   constructor() { 
     
   }
 
   ngOnInit() {
+  }
+
+  onSelectedRecipe(recipe: Recipe){
+    this.recSelected.emit(recipe);
   }
 
 }
--- my-course-project/src/app/shopping-list/shopping-list.component.ts
+++ my-course-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-list',
@@ -14,6 +15,10 @@ export class ShoppingListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
   }
 
 }
